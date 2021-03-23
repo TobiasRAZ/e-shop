584ce8fc0fcdcc30f829b8dd5a6fcd44632b12d6
JerryFalimanana <ratsimbajaona1006@gmail.com>
2021-03-23 12:24:54 +0000

    make entity user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     *
+     */
+    public function home() {
+
+        return $this->render('user/home.html.twig', [
+
+            'title' => "Tongasoa eto amin'ny site e-shop",
+            'soustitre' => "Mikonektiava ary eh !"
+
+        ]);
+
+    }
+
+}
